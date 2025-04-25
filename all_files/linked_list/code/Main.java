425eecea0fbc7da11c1f17b7a0009628aebd458c
Le Duy Toan Nguyen <104290101+sDuyToans@users.noreply.github.com>
2025-04-25 00:52:00 -0400

    finish singly linked list chapter

--- all_files/linked_list/code/Main.java
+++ all_files/linked_list/code/Main.java
@@ -4,14 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-        singlyLinkedList.createSingleLinkedList(5);
         // System.out.println(singlyLinkedList.head.value);
-        singlyLinkedList.insertInSingleLinkedList(6, 1);
-        singlyLinkedList.insertInSingleLinkedList(7, 3);
-        singlyLinkedList.insertInSingleLinkedList(8,4);
-        singlyLinkedList.insertInSingleLinkedList(9,0);
-        
+//        singlyLinkedList.insertInSingleLinkedList(6, 1);
+//        singlyLinkedList.insertInSingleLinkedList(7, 3);
+//        singlyLinkedList.insertInSingleLinkedList(8,4);
+//        singlyLinkedList.insertInSingleLinkedList(9,0);
+//
         // System.out.println(singlyLinkedList.head.next.next.value);
+        singlyLinkedList.push(5) ; // Success
+        singlyLinkedList.push(10);  // Success
+        singlyLinkedList.push(15);  // Success
+        singlyLinkedList.push(20);  // Success
+        singlyLinkedList.push(25);  // Success
+        singlyLinkedList.traverseSinglyLinkedList();
+        singlyLinkedList.rotate(3);
         singlyLinkedList.traverseSinglyLinkedList();
     }
 }
--- all_files/linked_list/code/SinglyLinkedList.java
+++ all_files/linked_list/code/SinglyLinkedList.java
@@ -9,15 +9,108 @@ public class SinglyLinkedList {
     public Node tail;
     public int size;
 
-    public Node createSingleLinkedList(int value) {
+    public Node insertSinglyLinkedList(int nodeValue) {
         head = new Node();
         Node node = new Node();
         node.next = null;
-        node.value = value;
+        node.value = nodeValue;
         head = node;
         tail = node;
         size = 1;
         return head;
+    }
+
+    // Insert Method SinglyLinkedList
+    public void push(int nodeValue) {
+        if (head == null) {
+            insertSinglyLinkedList(nodeValue);
+            return;
+        } else {
+            Node node = new Node();
+            node.value = nodeValue;
+            node.next = null;
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+
+
+    }
+
+    public Node pop() {
+        if (head == null) {
+            System.out.println("The SLL does not exist");
+            return null;
+        }
+        Node removeNode;
+        Node currentNode;
+        if (head == tail) {
+            removeNode = head;
+            head = tail = null;
+        } else {
+            currentNode = head;
+            while (currentNode.next != tail) {
+                currentNode = currentNode.next;
+            }
+            removeNode = currentNode.next;
+            currentNode.next = null;
+            tail = currentNode;
+        }
+        size--;
+
+        return removeNode;
+
+    }
+
+
+    //Get
+    public Node get(int index) {
+        if (index < 0 || index >= size){
+            return null;
+        }
+        Node current = this.head;
+        for (int i = 0; i < index; i++){
+            current = current.next;
+        }
+        return current;
+    }
+
+
+    //Insert
+    public boolean insert(int data, int index) {
+        Node newNode = new Node();
+        newNode.value = data;
+        if (index<0 || index >= size) {
+            return false;
+        }
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            if (index == 0) {
+                newNode.next = head;
+                head = newNode;
+            } else if (index == 1) {
+                newNode.next = head.next;
+                head.next = newNode;
+            } else if (index == size) {
+                tail.next = newNode;
+                newNode.next = null;
+                tail = newNode;
+            } else {
+                Node tempNode = head;
+                int inx = 0;
+                while (inx < index-1) {
+                    tempNode = tempNode.next;
+                    inx += 1;
+                }
+                Node nextNode = tempNode.next;
+                tempNode.next = newNode;
+                newNode.next = nextNode;
+            }
+        }
+        size +=1;
+        return true;
     }
 
     public void insertInSingleLinkedList(int nodeValue, int location) {
@@ -61,5 +154,66 @@ public class SinglyLinkedList {
             }
         }
         System.out.println("\n ");
+    }
+
+    //Rotate
+
+    public void rotate(int number) {
+        if (number < 0 || number >= size) {
+            return;
+        }
+        if (number == 0) {
+            return;
+        }
+        Node current = this.head;
+        for (int i = 0; i < number - 1; i++) {
+            current = current.next;
+        }
+        Node firstNode = current.next;
+        current.next = null;
+        Node temp = firstNode;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = this.head;
+        this.head = firstNode;
+        this.tail = current;
+    }
+
+    // Set
+
+    public boolean set(int index, int value) {
+        if (index < 0 || index >= size) {
+            return false;
+        }
+        if (head == null) {
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = null;
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        Node current = this.head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.value = value;
+        return true;
+    }
+
+    //Remove
+    public Node remove(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node current = this.head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        Node removeNode = current.next;
+        current.next = removeNode.next;
+        removeNode.next = null;
+        this.size -=1;
+        return removeNode;
     }
 }
