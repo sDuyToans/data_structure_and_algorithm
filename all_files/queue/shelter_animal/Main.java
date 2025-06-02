eff8ed861d97f0fbacdd642de11a268d194f8b35
Le Duy Toan Nguyen <104290101+sDuyToans@users.noreply.github.com>
2025-06-02 01:12:56 -0400

    with main code

--- all_files/queue/shelter_animal/Main.java
+++ all_files/queue/shelter_animal/Main.java
@@ -2,6 +2,16 @@ package all_files.queue.shelter_animal;
 
 public class Main {
     public static void main(String[] args) {
+        AnimalQueue animalQueue = new AnimalQueue();
+        animalQueue.enqueue(new Cat("Cat 1"));
+        animalQueue.enqueue(new Cat("Cat 2"));
+        animalQueue.enqueue(new Dog("Dog 1"));
+        animalQueue.enqueue(new Dog("Dog 2"));
+        animalQueue.enqueue(new Dog("Dog 3"));
+        animalQueue.enqueue(new Cat("Cat 3"));
 
+        System.out.println(animalQueue.dequeueAny().name);
+        System.out.println(animalQueue.dequeueDog().name);
+        System.out.println(animalQueue.dequeueDog().name);
     }
 }
